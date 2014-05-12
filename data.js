f1af644b33acfd1ae793d575641ebf3bae739a0f
Robot RG <zoorty@gmail.com>
2014-05-12 05:00:02 +0300

    new data

--- data.js
+++ data.js
@@ -41,7 +41,7 @@ var dataArray = [
 [new Date(1399849201000), 3315817, 3423787],
 [new Date(1399852801000), 3332845, 3433069],
 [new Date(1399856401000), 3349501, 3444297],
-[new Date(1399860001000), 3365800, undefined],
+[new Date(1399860001000), 3365800, 3456337],
 [new Date(1399863601000), 3381759, undefined],
 [new Date(1399867201000), 3397390, undefined],
 [new Date(1399870801000), 3412708, undefined],
@@ -161,4 +161,5 @@ var dataArray = [
 [new Date(1400281201000), 4315385, undefined],
 [new Date(1400284801000), 4320012, undefined],
 [new Date(1400288401000), 4324611, undefined],
+[new Date(1400292001000), 4329182, undefined],
 ]
