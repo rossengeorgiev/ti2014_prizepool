2b52ef377362d59f74a5320d7688a2887adaf1ae
Robot RG <zoorty@gmail.com>
2014-06-05 03:00:06 +0300

    new data

--- data.js
+++ data.js
@@ -626,8 +626,8 @@ rows: [
     { c:[ {v: new Date(1401912001000)}, {v: undefined}, {v: 8127315}, {v: ''}, {v: 7610579}, {v: ''}, {v: 8605441}, {v: ''},{v: 5266} ]},
     { c:[ {v: new Date(1401915601000)}, {v: undefined}, {v: 8131640}, {v: ''}, {v: 7613548}, {v: ''}, {v: 8610270}, {v: ''},{v: 4829} ]},
     { c:[ {v: new Date(1401919201000)}, {v: undefined}, {v: 8135959}, {v: ''}, {v: 7616513}, {v: ''}, {v: 8614106}, {v: ''},{v: 3836} ]},
-    { c:[ {v: new Date(1401922801000)}, {v: undefined}, {v: 8140275}, {v: ''}, {v: 7619473}, {v: ''}, {v: 8618075}, {v: '$8,618,075'},{v: 3969} ]},
-    { c:[ {v: new Date(1401926401000)}, {v: undefined}, {v: 8144585}, {v:''}, {v: 7622429}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
+    { c:[ {v: new Date(1401922801000)}, {v: undefined}, {v: 8140275}, {v: ''}, {v: 7619473}, {v: ''}, {v: 8618075}, {v: ''},{v: 3969} ]},
+    { c:[ {v: new Date(1401926401000)}, {v: undefined}, {v: 8144585}, {v: ''}, {v: 7622429}, {v: ''}, {v: 8617452}, {v: '$8,617,452'},{v: -623} ]},
     { c:[ {v: new Date(1401930001000)}, {v: undefined}, {v: 8148891}, {v:''}, {v: 7625380}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
     { c:[ {v: new Date(1401933601000)}, {v: undefined}, {v: 8153193}, {v:''}, {v: 7628327}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
     { c:[ {v: new Date(1401937201000)}, {v: undefined}, {v: 8157490}, {v:''}, {v: 7631270}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
@@ -747,4 +747,5 @@ rows: [
     { c:[ {v: new Date(1402347601000)}, {v: undefined}, {v: 8620203}, {v:''}, {v: 7940343}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
     { c:[ {v: new Date(1402351201000)}, {v: undefined}, {v: 8624045}, {v:''}, {v: 7942847}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
     { c:[ {v: new Date(1402354801000)}, {v: undefined}, {v: 8627883}, {v:''}, {v: 7945348}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
+    { c:[ {v: new Date(1402358401000)}, {v: undefined}, {v: 8631718}, {v:''}, {v: 7947845}, {v:''}, {v: undefined}, {v: ''}, {v: undefined} ]},
 ]}
